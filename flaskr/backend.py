db3842196420d34aa02361f3d5010bb4afd6da92
Bethany Abebe <bethanyabebe@techexchange.in>
2023-02-20 21:38:49 +0000

    started upbload, get_all_page_names functions

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -1,17 +1,25 @@
 # TODO(Project 1): Implement Backend according to the requirements.
+from google.cloud import storage
+
 class Backend:
 
-    def __init__(self):
-        pass
-        
+    def __init__(self, bucket_name):
+        self.bucket_name = bucket_name
+        self.storage_client = storage.Client()
+        self.bucket = self.storage_client.bucket(bucket_name)
+
     def get_wiki_page(self, name):
         pass
 
     def get_all_page_names(self):
-        pass
+        blobs = self.storage_client.list_blobs(self.bucket_name)
+        for blob in blobs:
+            print(blob.name)
 
-    def upload(self):
-        pass
+    def upload(self, page_name, page):
+        blob = self.bucket.blob(page_name)
+        with blob.open("w") as f:
+            f.write(page)
 
     def sign_up(self):
         pass
@@ -22,3 +30,6 @@ class Backend:
     def get_image(self):
         pass
 
+b = Backend("contentwiki")
+b.upload("warzone", "page")
+b.get_all_page_names()
